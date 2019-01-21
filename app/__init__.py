74e5ceb882cd942aa0b324a9e761dff26be1fbc9
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-01-20 18:27:39 -0600

    Adding sample code which downloads an excel file from google drive

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from app.GoogleSheetsService import parse_to_json
+from app.GoogleDriveService import download_and_parse_to_json
 from flask_jsonpify import jsonify
 
 app = Flask(__name__)
@@ -10,4 +11,9 @@ class Points(Resource):
     def get(self):
         return parse_to_json()
 
+class PointDrive(Resource):
+    def get(self):
+        return download_and_parse_to_json()
+
 api.add_resource(Points, '/points')
+api.add_resource(PointDrive, '/pointsdrive')
