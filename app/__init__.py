40eb9b1b9db73d184083977111171e68661a76e0
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-01-22 13:37:26 -0600

    Fixed CORS handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from flask_cors import cross_origin
+from flask_restful.utils import cors
 from app.GoogleSheetsService import GoogleSheetsService
 from app.GoogleDriveService import GoogleDriveService
 from flask_jsonpify import jsonify
@@ -11,12 +11,12 @@ google_sheets_service = GoogleSheetsService()
 google_drive_service = GoogleDriveService()
 
 class Points(Resource):
-    @cross_origin
+    @cors.crossdomain(origin='*', methods={"GET"})
     def get(self):
         type = request.args['source']
         if type == 'sheets':
-            return google_sheets_service.get_point_data()
+            return jsonify(google_sheets_service.get_point_data())
         elif type == 'drive':
-            return google_drive_service.get_point_data()
+            return jsonify(google_drive_service.get_point_data())
 
 api.add_resource(Points, '/points')
