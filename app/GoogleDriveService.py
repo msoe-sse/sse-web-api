47ced68adada5a38b92349bcc96fee30297c8a49
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-02-25 14:16:30 -0600

    Fixed an error in the GoogleDriveService

--- app/GoogleDriveService.py
+++ app/GoogleDriveService.py
@@ -16,7 +16,7 @@ class GoogleDriveService():
         self.drive_service = self._build_drive_service()
         workbook = self._load_workbook()
         result['meetings'] = self._parse_meetings(workbook.active.iter_rows(max_row=1, max_col=worksheet.max_column - 1))
-        result['students'] = self._parse_students(workbook.active.__le__iter_rows(min_row=2))
+        result['students'] = self._parse_students(workbook.active.iter_rows(min_row=2))
         return result
 
     def _load_workbook(self):
