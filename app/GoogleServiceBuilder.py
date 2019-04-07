96a0e99ea1cec23cdd17543163b2b3a0abd4c7b1
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-04-07 18:52:19 -0500

    Renamed the build_credentials method back to its old name

--- app/GoogleServiceBuilder.py
+++ app/GoogleServiceBuilder.py
@@ -14,7 +14,7 @@ class GoogleServiceBuilder():
         credentials = self._build_credentials(scopes)
         return build(service_name, service_version, credentials=credentials, developerKey=os.environ.get("API_KEY"))
 
-    def _build_credentials(self, scopes):
+    def build_credentials(self, scopes):
         credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
         service_account_info = json.loads(credentials_raw)
         return service_account.Credentials.from_service_account_info(service_account_info, scopes=scopes)
