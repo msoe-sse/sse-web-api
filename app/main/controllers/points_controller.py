dd4c3b3469f5a0a9db1d35d023a0d946f3f0578c
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-07-25 19:02:16 -0500

    Initial reorg of project and bringing old code up to Python standards

---
+++ app/main/controllers/points_controller.py
@@ -0,0 +1,17 @@
+from flask_restplus import Resource
+from ..points.google_sheets_points_service import get_point_data
+from ..points.points_dto import PointsDto
+
+api = PointsDto.api
+_points = PointsDto.points
+
+@api.route('/points')
+class Points(Resource):
+    def __init__(self):
+        self.sheets_service = GoogleSheetsPointsService()
+
+    #@cors.crossdomain(origin='*', methods={"GET"})
+    @api.doc('gets the list of point data for a quarter')
+    @api.marshal_list_with(_points, envelope='data')
+    def get(self):
+        return get_point_data()
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from app import app
+import unittest
 
+from app.main import create_app
+from flask_script import Manager
+
+
+app = create_app()
+
+@manager.command
+def run():
+     app.run()
+
+@manager.command
+def test():
+     tests = unittest.TestLoader().discover('app/main/test', pattern='*_test.py')
+     result = unittest.TextTestRunner(verbosity=2).run(tests)
+     if result.wasSuccessful():
+          return 0
+     return 1
+     
 if __name__ == '__main__':
      app.run()
