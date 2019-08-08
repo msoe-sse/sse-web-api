e432589c1cbb7c07d05419e9287cdb55b1328163
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-08-08 18:28:58 -0500

    Implemented the resources controller and the resources dto

---
+++ app/main/controllers/resources_controller.py
@@ -0,0 +1,14 @@
+from flask_restplus import Resource
+from ..resources.resource_service import get_all_resources
+from ..resources.resources_dto import ResourcesDto
+
+api = ResourcesDto.api
+_resources = ResourcesDto.points
+
+@api.route('/')
+class Resources(Resource):
+    #@cors.crossdomain(origin='*', methods={"GET"})
+    @api.doc('gets the list of all SSE resources')
+    @api.marshal_list_with(_resources, envelope='data')
+    def get(self):
+        return get_all_resources()
