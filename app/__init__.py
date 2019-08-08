52ec14462370c851437ed7a98da5fe1d9ba96563
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-08-08 18:31:46 -0500

    Adding resources to the api initialization

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask_restplus import Api
 from flask import Blueprint
 
 from .main.controllers.points_controller import api as points_ns
+from .main.controllers.resources_controller import api as resources_ns
 
 blueprint = Blueprint('api', __name__)
 
@@ -11,3 +12,4 @@ api = Api(blueprint,
           description='A API used for the MSOE Society of Software Engineers Website')
 
 api.add_namespace(points_ns, path='/points')
+api.add_namespace(resources_ns, path='/resources')
