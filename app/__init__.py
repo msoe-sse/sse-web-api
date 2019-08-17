6613d8c20d652c343bb46a6c2cff4176602151d5
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-08-17 17:54:37 -0500

    Forgot to save file

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,8 @@ from flask import Blueprint
 from flask_cors import CORS
 
 from .main.controllers.points_controller import api as points_ns
-<<<<<<< HEAD
 from .main.controllers.resources_controller import api as resources_ns
-=======
 from app.main import create_app
->>>>>>> 122393e9f080aa59b12151dcf159eaca108ae9e4
 
 blueprint = Blueprint('api', __name__)
 
@@ -17,9 +14,7 @@ api = Api(blueprint,
           description='A API used for the MSOE Society of Software Engineers Website')
 
 api.add_namespace(points_ns, path='/points')
-<<<<<<< HEAD
 api.add_namespace(resources_ns, path='/resources')
-=======
 
 app = create_app()
 app.register_blueprint(blueprint)
@@ -27,4 +22,3 @@ app.register_blueprint(blueprint)
 app.app_context().push()
 
 CORS(app, resources={r"/*": {"origins": "*"}})
->>>>>>> 122393e9f080aa59b12151dcf159eaca108ae9e4
